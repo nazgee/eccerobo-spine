32b2c294bf4ff871b81da19c538b397cfb9eb8d0
Michal Stawinski <michal.stawinski@gmail.com>
2012-09-02 21:55:43 +0200

    Removed some dead code

--- spine.cpp
+++ spine.cpp
@@ -35,8 +35,5 @@ int main(int argc, char **argv) {
 	SpineServer server = SpineServer(modbus, osock::AuthNone::Populate(), "10237");
 	server.Start();
 
-
-//	mb.testrun();
-
 	return EXIT_SUCCESS;
 }
